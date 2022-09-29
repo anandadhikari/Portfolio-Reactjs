37e29e342fbde484f997c20473595018c0b8a9cd
anandadhikari <anandadhikari02@gmail.com>
2022-09-30 01:28:45 +0530

    fix

--- src/app/App.js
+++ src/app/App.js
@@ -4,14 +4,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HelmetMeta } from "./HelmetMeta";
 import { ThemeProvider } from "../components/theme/ThemeProvider";
 import { CssBaseline } from "@material-ui/core";
-import { logCredits } from "../utils/logCredits";
 
 import { Home } from "../pages/Home";
 
 const PageNotFound = lazy(() => import("../pages/PageNotFound"));
 
 export const App = () => {
-    logCredits();
 
     return (
       <ThemeProvider>
