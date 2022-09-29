d14925d26a24afe8529e53aaad2bebfaabbfd977
anandadhikari <anandadhikari02@gmail.com>
2022-09-30 02:29:15 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React, { Suspense } from "react";
+import React from "react";
+import { render } from "react-snapshot";
 import { App } from "./app/App";
 import "./index.css";
-import { hydrate, render } from "react-dom";
- 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<Suspense fallback={<div>Loading... </div>}>
-  <App />
-</Suspense>, rootElement);
-} else {
-  render(<Suspense fallback={<div>Loading... </div>}>
-  <App />
-</Suspense>, rootElement);
-}
+
+render(<App />, document.getElementById("root"));
